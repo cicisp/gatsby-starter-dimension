bf8e02bd0546e2e31fa7125721252a203570b194
cicisp <ceciliaspruijt@gmail.com>
2021-10-14 20:49:20 +0200

    added page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,9 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Hi!</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Welcome to my portfolio website!
         </p>
       </div>
     </div>
@@ -54,6 +50,16 @@ const Header = props => (
             }}
           >
             Contact
+          </button>
+        </li>
+
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('blog')
+            }}
+          >
+            Blog
           </button>
         </li>
       </ul>
