57fc8ed32d6afef4b41e5e9ef6e2b87e2d24fe9a
Cicisp <cspruijt@cardia.nl>
2021-11-08 17:02:46 +0100

    change heading

--- src/components/Main.js
+++ src/components/Main.js
@@ -100,7 +100,7 @@ class Main extends React.Component {
         >
           <h2 className="major">Reading List</h2>
           
-          <iframe allowtransparency="true" style="background-color: #989898; color: #fffefe" src = "https://csp.libib.com" width="100%" height="500"> </iframe>
+          <iframe  src = "https://csp.libib.com" allowtransparency="true" style="background-color:transparent" width="50%" height="500"> </iframe>
            {close}
         </article>
 
